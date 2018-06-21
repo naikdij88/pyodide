8c3c1833f84566a91f70e10c3ca7e3d41f34f087
Sarah Bird <birdsarah@mozilla.com>
2018-06-20 22:56:13 -0500

    Display output and scroll

--- addon/src/pyconsole.js
+++ addon/src/pyconsole.js
@@ -1,16 +1,50 @@
+"use strict";
+
+let scrollCount = 0;
+let pyinput = document.getElementById('pyinput');
+
 function submit() {
-    let text = document.getElementById("pyinput").value;
-    let response = pyodide.runPython(text);
-    console.log(response);
-};
+    let output = document.getElementById("output");
+    let input = pyinput.value;
 
-function enterPressed() {
-    let input = document.getElementById('pyinput');
-    input.addEventListener('keyup', (event) => {
-        if (event.which == 13) {
-            submit();
+    // Setup output display
+    let result = document.createElement('div');
+    result.className = 'result';
+
+    let displayInput = document.createElement('div');
+    displayInput.className = 'display-input';
+    displayInput.appendChild(document.createTextNode(input));
+
+    result.appendChild(displayInput);
+
+    // Run python
+    try {
+        let response = pyodide.runPython(input);
+        if (response !== undefined) {
+            let displayOutput = document.createElement('pre');
+            displayOutput.className = 'display-output';
+            displayOutput.appendChild(document.createTextNode(response));
+            result.appendChild(displayOutput);
         }
-    });
+    }
+    catch(error) {
+        let displayError = document.createElement('pre');
+        displayError.className = 'display-error';
+        displayError.appendChild(document.createTextNode(error.message));
+        result.appendChild(displayError);
+    }
+
+    // Display it
+    output.appendChild(result);
+    scrollCount = scrollCount + result.getBoundingClientRect().height;
+    output.scrollTop = scrollCount;
+
+    // Finally clear input so we can go again
+    pyinput.value = '';
 };
 
-enterPressed();
+pyinput.addEventListener('keyup', (event) => {
+    if (event.which == 13) {
+        submit();
+    }
+});
