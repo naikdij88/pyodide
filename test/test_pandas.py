6e236d0ec73c74ec3df97881f9c881016ad3899d
Sarah Bird <birdsarah@mozilla.com>
2018-06-20 19:52:38 -0500

    Big remove - just back to cpython

--- test/test_pandas.py
+++
@@ -1,8 +0,0 @@
-def test_pandas(selenium):
-    selenium.load_package("pandas")
-    assert len(selenium.run("import pandas\ndir(pandas)")) == 179
-
-
-def test_extra_import(selenium):
-    selenium.load_package("pandas")
-    selenium.run("from pandas import Series, DataFrame, Panel")
